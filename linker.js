7184e22e10980355b343b5adf45dd404d30f6344
lemon-mint <55233766+lemon-mint@users.noreply.github.com>
2021-01-19 08:52:11 +0900

    add print

--- linker.js
+++ linker.js
@@ -3,28 +3,32 @@ const runlinker = () => {
         hljs.initHighlighting();
         document.querySelectorAll("a").forEach((el) => {
             if (typeof (el.href) == "string") {
-                if (((el.href.startsWith("http://") || el.href.startsWith("https://")) && (el.origin != window.location.origin)) && !allowlist.includes(el.host)) {
-                    el.rel = "nofollow";
-                    el.onclick = (e) => {
-                        e.preventDefault();
-                        let clickcount = e.detail;
-                        setTimeout(() => {
-                            console.log(clickcount);
-                            if (clickcount < 2) {
-                                window.location.href = "/redirect/?url=" + el.href;
-                            } else {
+                if ((el.href.startsWith("http://") || el.href.startsWith("https://")) && (el.origin != window.location.origin)) {
+                    console.log("Found :", el.href);
+                    if (!allowlist.includes(el.host)) {
+                        console.log("Host", el.host, "Nofollow :", el.href);
+                        el.rel = "nofollow";
+                        el.onclick = (e) => {
+                            e.preventDefault();
+                            let clickcount = e.detail;
+                            setTimeout(() => {
+                                console.log(clickcount);
+                                if (clickcount < 2) {
+                                    window.location.href = "/redirect/?url=" + el.href;
+                                } else {
+                                    window.location.href = el.href;
+                                }
+                            }, 100)
+                            return false;
+                        }
+                        el.ondblclick = (e) => {
+                            e.preventDefault();
+                            setTimeout(() => {
                                 window.location.href = el.href;
-                            }
-                        }, 100)
-                        return false;
+                            }, 10)
+                            return false;
+                        };
                     }
-                    el.ondblclick = (e) => {
-                        e.preventDefault();
-                        setTimeout(() => {
-                            window.location.href = el.href;
-                        }, 10)
-                        return false;
-                    };
                 }
             }
         });
