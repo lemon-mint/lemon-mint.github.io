055a0a6a1514b751d1504205c7d81c399c7bcb7a
lemon-mint <55233766+lemon-mint@users.noreply.github.com>
2021-01-19 08:54:29 +0900

    fix

--- post.js
+++ post.js
@@ -57,35 +57,8 @@ const main_document = {
         rawGET("body.md", (data) => {
           objs.markdown = md.makeHtml(data);
           setTimeout(() => {
-            hljs.initHighlighting();
-            document.querySelectorAll("a").forEach((el) => {
-              if (typeof (el.href) == "string") {
-                if ((el.href.startsWith("http://") || el.href.startsWith("https://")) && (el.origin != window.location.origin)) {
-                  el.rel = "nofollow";
-                  el.onclick = (e) => {
-                    e.preventDefault();
-                    let clickcount = e.detail;
-                    setTimeout(() => {
-                      console.log(clickcount);
-                      if (clickcount < 2) {
-                        window.location.href = "/redirect/?url=" + el.href;
-                      } else {
-                        window.location.href = el.href;
-                      }
-                    }, 100)
-                    return false;
-                  }
-                  el.ondblclick = (e) => {
-                    e.preventDefault();
-                    setTimeout(() => {
-                      window.location.href = el.href;
-                    }, 10)
-                    return false;
-                  };
-                }
-              }
-            });
-          }, 500);
+            runlinker();
+          }, 100);
         });
       }, 400
     )
