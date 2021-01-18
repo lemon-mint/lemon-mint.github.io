031f1409472982a69d9be364c654be6f39561ecc
lemon-mint <55233766+lemon-mint@users.noreply.github.com>
2021-01-18 18:15:25 +0900

    Add Markdown

---
+++ body.md
@@ -0,0 +1,11 @@
+- [x] checked
+- [ ] blank
+
+[google link](https://google.com)
+
+[local link](/home)
+
+``` javascript
+//Code block
+console.log("Hello, World!");
+```
--- index.js
+++ index.js
@@ -1,29 +1,87 @@
+let md = new showdown.Converter();
+md.setFlavor("github")
+md.setOption('tasklists', 'true');
+md.setOption('tables', 'true');
+md.setOption('parseImgDimensions', 'true');
+md.setOption('ghCodeBlocks', 'true');
+md.setOption('requireSpaceBeforeHeadingText', 'true');
+
 async function jsonGET(url, callback) {
-    let xhr = new XMLHttpRequest();
-    xhr.open("GET", url);
-    xhr.onreadystatechange = () => {
-        if (xhr.readyState == 4) {
-            let data = JSON.parse(xhr.responseText);
-            callback(data);
-        }
-    };
-    xhr.send();
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", url);
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState == 4) {
+      let data = JSON.parse(xhr.responseText);
+      callback(data);
+    }
+  };
+  xhr.send();
+}
+
+async function rawGET(url, callback) {
+  let xhr = new XMLHttpRequest();
+  xhr.open("GET", url);
+  xhr.onreadystatechange = () => {
+    if (xhr.readyState == 4) {
+      let data = xhr.responseText;
+      callback(data);
+    }
+  };
+  xhr.send();
 }
 
 const main_document = {
-    data() {
-        return {
-            navtitle: "Loading...",
-            title: "Loading..."
-        }
-    },
-    mounted() {
-        let objs = this 
-        jsonGET("/index.json",(data)=>{
-            objs.navtitle = data.navtitle;
-            objs.title = data.title;
+  data() {
+    return {
+      navtitle: "Loading...",
+      title: "Loading...",
+      markdown: "<p>Loading...</p>"
+    }
+  },
+  mounted() {
+    let objs = this;
+    jsonGET("/index.json", (data) => {
+      objs.navtitle = data.navtitle;
+    });
+    jsonGET("meta.json", (data) => {
+      objs.title = data.title;
+    });
+    setTimeout(
+      () => {
+        rawGET("body.md", (data) => {
+          objs.markdown = md.makeHtml(data);
+          setTimeout(() => {
+            hljs.initHighlighting();
+            document.querySelectorAll("a").forEach((el) => {
+              if (typeof (el.href) == "string") {
+                if ((el.href.startsWith("http://") || el.href.startsWith("https://")) && (el.origin != window.location.origin)) {
+                  el.rel = "nofollow";
+                  el.onclick = (e) => {
+                    let clickcount = e.detail;
+                    setTimeout(() => {
+                      console.log(clickcount);
+                      if (clickcount < 2) {
+                        window.location.href = "/redirect/?url=" + el.href;
+                      } else {
+                        window.location.href = el.href;
+                      }
+                    }, 100)
+                    return false;
+                  }
+                  el.ondblclick = () => {
+                    setTimeout(() => {
+                      window.location.href = el.href;
+                    }, 10)
+                    return false;
+                  };
+                }
+              }
+            });
+          }, 500);
         });
-    },
-    delimiters: ['[%^]', '[^%]']
+      }, 400
+    )
+  },
+  delimiters: ['[%^]', '[^%]']
 };
 Vue.createApp(main_document).mount('#main-document');
